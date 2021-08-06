0ac2b33c7f9dc1b59b6c9bcf5bbd6350c7f5d8f5
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-08-05 21:35:26 -0500

    commits servlet integration

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -1,12 +1,16 @@
 import com.google.gson.Gson;
+import data.DaoFactory;
 import data.Movie;
+import data.MoviesDao;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name = "MovieServlet", urlPatterns = "/movies")
 public class MovieServlet extends HttpServlet {
@@ -19,11 +23,16 @@ public class MovieServlet extends HttpServlet {
             //get
             PrintWriter out = response.getWriter();
             // Eventually get movies form the database
-            Movie movie = new Movie(2, "Tenet", "2021", "5", "Armed with only one word, Tenet," +
-                    " and fighting for the survival of the entire world, a Protagonist journeys\n" +
-                    "through a twilight world of international espionage on a mission that will unfold in something beyond real time.");
+
+//            Movie movie = new Movie(2, "Tenet", "2021", "5", "Armed with only one word, Tenet," +
+//                    " and fighting for the survival of the entire world, a Protagonist journeys\n" +
+//                    "through a twilight world of international espionage on a mission that will unfold in something
+//                    beyond real time.");
+
+            MoviesDao moviesdao = DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY);
+
            //turn into a json string
-            String movieString = new Gson().toJson(movie);
+            String movieString = new Gson().toJson(moviesdao.all());
 
             //inject into response
             out.println(movieString);
@@ -54,11 +63,17 @@ public class MovieServlet extends HttpServlet {
                 System.out.println(movie.getRating());
                 System.out.println(movie.getPlot());
                 System.out.println("==========================");
+
+                DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).insertAll(movies);
             }
 
-        }catch (Exception ex){
-            System.out.println(ex.getMessage());
+        }catch (SQLException | IOException e){
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(500);
+            e.printStackTrace();
+            return;
         }
+
 
         // write meaningful responses and set the response to 200
         out.println(new Gson().toJson("{message: \"Movies POST was successful\"}"));
@@ -91,6 +106,21 @@ public class MovieServlet extends HttpServlet {
             System.out.println(ex.getMessage());
         }
 
+        try {
+            Movie movie = new Gson().fromJson(request.getReader(), Movie.class);
+            DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).update(movie);
+        } catch (SQLException e) {
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(500);
+            e.printStackTrace();
+            return;
+        } catch (Exception e) {
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(400);
+            e.printStackTrace();
+            return;
+        }
+
         out.println(new Gson().toJson("{message: \"Movies PUT was successful\"}"));
         response.setStatus(200);
     }
@@ -111,6 +141,16 @@ public class MovieServlet extends HttpServlet {
             System.out.println("id: " + id + " has been deleted");
         } catch (Exception ex){
             System.out.println(ex.getMessage());
+        }
+
+        try {
+            var id = new Gson().fromJson(request.getReader(), int.class);
+            DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).destroy(id);
+        } catch (SQLException | IOException e) {
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(500);
+            e.printStackTrace();
+            return;
         }
 
         out.println(new Gson().toJson("{message: \"Movies DELETE was successful\"}"));
---
+++ src/main/java/data/DaoFactory.java
@@ -0,0 +1,21 @@
+package data;
+
+
+public class DaoFactory {
+
+    private static MoviesDao moviesDao;
+//    private static Config config = new Config();
+
+    public enum ImplType {MYSQL, IN_MEMORY}; //Notice we have two values here
+
+    public static MoviesDao getMoviesDao(ImplType implementationType) {
+
+        switch (implementationType) {
+            case IN_MEMORY: { //yet we have one switch case. We'll get to that!
+                moviesDao = new InMemoryMoviesDao();
+            }
+        }
+        return moviesDao;
+    }
+}
+
